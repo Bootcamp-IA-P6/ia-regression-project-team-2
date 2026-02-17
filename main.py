3d37808f79dade409e68693425aef8c7bbc14d59
iRuperth <ruperthlosada@gmail.com>
2026-02-17 12:18:02 +0100

    .

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 def main():
+<<<<<<< HEAD
     print("IA Regression Project Team 2")
+=======
+    print("Hello from proyecto5-regresion!")
+>>>>>>> 2df67593ce408594d86c93359bef734876e5ad46
 
 
 if __name__ == "__main__":
